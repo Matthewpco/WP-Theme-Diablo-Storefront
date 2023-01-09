bd7aea11f65acd51d66b98adb3ac4a5fd9bac342
Reese <65929791+bettameta@users.noreply.github.com>
2023-01-08 20:39:56 -0800

    Added Footer content and CSS styles to match Diablo-4 website.

--- footer.php
+++ footer.php
@@ -9,16 +9,55 @@
 
 ?>
 
-		
+<!-- Footer content begin -->
+	<footer id="main-footer"  role="contentinfo">	
+		<div class="container" style="text-align: center; background: #000; color: #B0AFAB;">
 
-	<footer id="" class="" role="contentinfo">
-		<div class="">
-			TODO Reese feature: D4S-2 <a href="https://www.notion.so/D4S-2-1d546517eefb49afaf8ab4ad8ac1c5de">>Notion task link<</a>
+			<!-- Footer logo -->							
+			<div class="footer-logo">
+				<img src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/cropped-Blizzard_Entertainment_Logo-e1672944594113-1-e1673155978548.webp" alt="logo">
+			</div>
+			<!-- Footer nav to be replaced globally -->
+			<div class="footer-nav">
+				<h5 style="color: #B0AFAB;"><a href="">CAREERS</a>  | <a href="">ABOUT</a>  | <a href="">SUPPORT</a>  | <a href="">CONTACT US</a>  | <a href="">API</a></h5>
+			</div>
+
+			<div class="all-games">
+				<p >All games, one app: <a href="https://www.blizzard.com/apps/battle.net/desktop">Battle.net Desktop App</a>
+				</p>
+			</div>
+
+			<div class="copyright">
+				<p>&copy <script>document.write( new Date().getFullYear() );</script> BLIZZARD ENTERTAINMENT, INC. ALL RIGHTS RESERVED.<br>All <span>trademarks</span> referenced herin are the properties of thier respective owners.<br><span>Do not sell or share my personal information</span></p>
+				
+				<h6 style="color: #B0AFAB;"><a href="">PRIVACY</a> | <a href="">LEGAL</a> | <a href="">TERMS</a> | <a href="">COOKIE POLICY</a> | <a href="">COOKIE SETTINGS</a></h6>
+			</div>
+
+			<!-- images and text should be inline block-->
+			<div class="footer-legal">
+				<div class="legal-container">
+					<img id="esrb-policy" src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/esrb.webp" alt="esrb-policy">
+					<img id="esrb-mature" src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/m.1BK7T.webp" alt="ESRB-mature">
+					<ul style="list-style: none;">
+						<li>Blood and Gore</li>
+						<li>Intense Violence</li>
+						<li>Language</li>
+					</ul>
+				</div>
+			</div>
 		</div>
-	</footer><!-- #footer -->
-</div><!-- #full-page -->
+<div class="" style="background-color: #232323; padding-left: 1%;">
+TODO Reese feature: D4S-2 <a href="https://www.notion.so/D4S-2-1d546517eefb49afaf8ab4ad8ac1c5de">Notion task link</a>
+</div>
+	</footer>
+<!-- #footer -->
+<!-- #full-page -->
 
 <?php wp_footer(); ?>
 
 </body>
 </html>
+
+
+
+
