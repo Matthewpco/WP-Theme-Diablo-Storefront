fc106e586089ab1df0b10af4719567fad6eaf9b0
Matthew Payne <matthewpco86@gmail.com>
2023-01-21 20:52:31 -0800

    feature/D4S-5: added responsive mobile styles to web component

--- assets/js/wc-classes.js
+++ assets/js/wc-classes.js
@@ -79,6 +79,37 @@ export class d4sClasses extends LitElement {
         width: 140%;
         margin-left: -30%;
     }
+
+    /* Mobile media queries */
+    @media only screen and (max-width: 767px) {
+        video, iframe {
+
+            width: 300px;
+            height: 198px;
+
+        }
+        .row {
+            flex-wrap: wrap;
+        }
+
+        .one-third-section {
+            flex-direction: column;
+            width: 100%
+
+        }
+
+        .two-thirds-section {
+            display: none;
+        }
+
+        #mob-img-1,#mob-img-2,#mob-img-3,#mob-img-4,#mob-img-5 {
+            display: flex;
+        }
+        .wc-img {
+            margin: -40% 0 -30% 0;
+            width: 100%;
+        }
+    }
   `;
 
   constructor() {
@@ -89,11 +120,11 @@ export class d4sClasses extends LitElement {
     this.description =
       "The Barbarian has unparalleled strength and expertly wields an entire arsenal in battle, with a weapon for every occasion.";
     this.image =
-      "http://garyp38.sg-host.com/wp-content/uploads/2023/01/d4-class-bg-parallax-mobile-barbarian-001.webp";
+      "h/wp-content/uploads/2023/01/d4-class-bg-parallax-mobile-barbarian-001.webp";
     this.video =
-      "http://garyp38.sg-host.com/wp-content/uploads/2023/01/lg-barb.webm";
+      "/wp-content/uploads/2023/01/lg-barb.webm";
     this.icon =
-      "http://garyp38.sg-host.com/wp-content/uploads/2023/01/d4-classes-icon-barbarian-base.webp";
+      "/wp-content/uploads/2023/01/d4-classes-icon-barbarian-base.webp";
   }
 
   // The render() method is called any time reactive properties change.
@@ -107,9 +138,10 @@ export class d4sClasses extends LitElement {
 
             <div class="row" id="dropdown-toggle-1" @click=${this.showBarbarian} style="z-index: 2">
                 <img src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/d4-classes-icon-barbarian-base.webp" width="40px" height="40px" />
-                <h3 class="color-dark-gray">BARBARIAN</h3>
+                <h3 class="color-dark-gray">BARBARIAN</h3> 
             </div>
             <div id="dropdown-content-1" class="">
+            <img id="mob-img-1" class="wc-img hidden" src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/d4-class-bg-parallax-mobile-barbarian-001-1.webp"  />
                 <p class="color-gray">The Barbarian has unparalleled strength and expertly wields an entire arsenal in battle, with a weapon for every occasion. He bellows intimidating war cries and unleashes ground-shaking slams to send approaching hordes reeling.</p>
                 <video width="480" height="270" controls poster="http://garyp38.sg-host.com/wp-content/uploads/2023/01/barb-overlay.webp">
                     <source src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/lg-barb.webm" type="video/webm">
@@ -120,8 +152,10 @@ export class d4sClasses extends LitElement {
             <div class="row" id="dropdown-toggle-2" @click=${this.showDruid}>
                 <img src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/d4-classes-icon-druid-base.webp" width="40px" height="40px" />
                 <h3 class="color-dark-gray">DRUID</h3>
+
             </div>
             <div id="dropdown-content-2" class="hidden">
+            <img id="mob-img-2" class="wc-img hidden" src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/d4-class-bg-parallax-mobile-druid-001.webp" />
                 <p class="color-gray">The Druid is a savage shapeshifter, fluidly transforming between the forms of a towering bear or a vicious werewolf to fight alongside the creatures of the wild. He also commands the power of earth, wind, and storm, unleashing nature’s wrath to devastating effect.</p>
                 <video width="480" height="270"  controls poster="http://garyp38.sg-host.com/wp-content/uploads/2023/01/druid-overlay.webp">
                     <source src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/lg-druid.webm" type="video/webm">
@@ -134,6 +168,7 @@ export class d4sClasses extends LitElement {
                 <h3 class="color-dark-gray">NECROMANCER</h3>
             </div>
             <div id="dropdown-content-3" class="hidden">
+            <img id="mob-img-3" class="wc-img hidden" src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/d4-class-bg-parallax-mobile-necromancer-001.webp" />
                 <p class="color-gray">Necromancers are cunning summoners that conjure vengeful hordes of the undead. Their Essence flows into three powerful bastions of Bone, Blood, or Shadow to bring low their enemies.</p>
                 <video width="480" height="270"   controls poster="http://garyp38.sg-host.com/wp-content/uploads/2023/01/necromancer-overlay.webp">
                     <source src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/D4_Necromancer_Trailer_Video_Asset.webm" type="video/webm">
@@ -146,6 +181,7 @@ export class d4sClasses extends LitElement {
                 <h3 class="color-dark-gray">ROGUE</h3>
             </div>
             <div id="dropdown-content-4" class="hidden">
+            <img id="mob-img-4" class="wc-img hidden" src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/d4-class-bg-parallax-mobile-rogue-001.webp" />
                 <p class="color-gray">The Rogue is an adaptable, agile warrior who can specialize in ranged or close quarters combat. She can best any foe with her imbued weapons, perform powerful combo attacks, and can augment her arsenal with deadly poisons and shadow magic to slay demons with impunity.</p>
                 <iframe width="480" height="270"  src="https://www.youtube.com/embed/LvrLZ4yETHI" title="Diablo IV - Rogue Announce Trailer" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
             </div>
@@ -155,6 +191,7 @@ export class d4sClasses extends LitElement {
                 <h3 class="color-dark-gray">SORCERER</h3>
             </div>
             <div id="dropdown-content-5" class="hidden">
+            <img id="mob-img-5" class="wc-img hidden" src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/d4-class-bg-parallax-mobile-sorcerer-001.webp" />
                 <p class="color-gray">The Sorceress shapes the elements into whatever form is necessary to ensure victory, from hurling bolts of lightning, impaling her foes upon jagged spikes of ice, and raining flaming meteors down from the sky.</p>
                 <video width="480" height="270"  controls poster="http://garyp38.sg-host.com/wp-content/uploads/2023/01/sorcerer-overlay.webp">
                     <source src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/lg-sorc.webm" type="video/webm">
@@ -302,7 +339,5 @@ export class d4sClasses extends LitElement {
     this._sectionImg2.classList.add("hidden");
   }
 }
-
-console.log("LOADED");
 
 customElements.define("d4s-classes", d4sClasses);
