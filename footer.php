a7c6057336bf51dd5682500cfc0fa6360c00f0e6
Reese <65929791+bettameta@users.noreply.github.com>
2023-01-24 09:34:27 -0800

    Added styles back to the footer branch and added social section. Have questions regarding colors and img assets.

--- footer.php
+++ footer.php
@@ -1,28 +1,38 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- *
- * @package storefront
- */
-
-?>
-
-
-<!--	<div class="">
-			TODO Reese feature: D4S-2 <a href="https://www.notion.so/D4S-2-1d546517eefb49afaf8ab4ad8ac1c5de">Notion task link</a>
-	</div>	
--->
 <!-- Footer content begin -->
-	<footer id="main-footer"  role="contentinfo">
-		<div class="container" style="text-align: center; background: #000; color: #B0AFAB;">
-
+<footer id="main-footer"  role="contentinfo">	
+		<div class="container">
+		<!-- Social Media Section -->
+		<div class="bliz-stay-connect">
+			<h3>Stay Connected</h3>
+		<!-- Social Icons -->
+			<div class="bliz-social-icons">
+				<ul>
+					<li><a href="https://www.facebook.com/Diablo" target="_blank"><i class="fab fa-facebook"></i></a></li>
+					<li><a href="https://twitter.com/Diablo" target="_blank"><i class="fab fa-twitter"></i></a></li>
+					<li><a href="https://www.youtube.com/Diablo" target="_blank"><i class="fab fa-youtube"></i></a></li>
+				</ul>
+			</div>
+		</div>
+		<!-- Language Selector -->
+		<div>
+			<form class="languages">
+			<label for="language">Language:</label>
+			<select name="language" id="language">
+				<option value="volvo">English (US)</option>
+				<option value="volvo">English (UK)</option>
+				<option value="volvo">Japanese</option>
+				<option value="volvo">German</option>
+				<option value="volvo">Russian</option>
+				<option value="saab">Spanish</option>
+				<option value="opel">French</option>
+				<option value="audi">Chinese</option>
+			</select>
+			</form>
+		</div>
 			<!-- Footer logo -->							
 			<div class="footer-logo">
-				<img src="http://garyp38.sg-host.com/wp-content/uploads/2023/01/cropped-Blizzard_Entertainment_Logo-e1672944594113-1-e1673155978548.webp" alt="logo">
+				<a href="https://blizzard.com/"  target="_blank" ><img src="http://diablo41.local/wp-content/uploads/2023/01/cropped-Blizzard_Entertainment_Logo-e1672944594113.webp"alt="logo"></a>
 			</div>
-
 			<div class="top-nav">
 				<nav>
 					<ul class="upper-nav-ul">
@@ -35,12 +45,10 @@
 					</ul>
 				</nav>
 			</div>
-
 			<div class="all-games">
-				<p >All games, one app: <a href="https://www.blizzard.com/apps/battle.net/desktop">Battle.net Desktop App</a>
+				<p>All games, one app: <a href="https://www.blizzard.com/apps/battle.net/desktop"><img src="http://diablo41.local/wp-content/uploads/2023/01/ezgif.com-gif-maker.webp"> Battle.net Desktop App</a>
 				</p>
 			</div>
-
 			<div class="copyright">
 				<p>&copy 2023 BLIZZARD ENTERTAINMENT, INC. ALL RIGHTS RESERVED.<br>All <span>trademarks</span> referenced herin are the properties of thier respective owners.<br><span>Do not sell or share my personal information</span></p>
 				<nav>
@@ -53,26 +61,21 @@
 					</ul>
 				</nav>
 			</div>
-
-			<!-- images and text should be inline block-->
 			<div class="footer-legal">
 				<div class="legal-container">
 					<img id="esrb-policy" src="/wp-content/uploads/2023/01/esrb.webp" alt="esrb-policy">
-					<img id="esrb-mature" src="/wp-content/uploads/2023/01/m.1BK7T.webp" alt="ESRB-mature">
-					<ul style="list-style: none;">
+					<img id="esrb-mature" src="http://diablo41.local/wp-content/uploads/2023/01/m.1BK7T.webp" alt="ESRB-mature">
+					<ul>
 						<li>Blood and Gore</li>
 						<li>Intense Violence</li>
 						<li>Language</li>
 					</ul>
 				</div>
 			</div>
-			
-		<div>
+		</div>
 	</footer>
 <!-- #footer -->
 <!-- #full-page -->
-
 <?php wp_footer(); ?>
-
 </body>
 </html>
